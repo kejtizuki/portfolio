4264018140384c5c1423a49dd010ad84c9414883
Kasia <kaz@issuu.com>
2020-07-31 14:16:28 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0f5419ddc691b020f43d86bb67add46e.js"
+  "/portfolio/precache-manifest.7062e442bb8a385bd0a55703525b8428.js"
 );
 
 workbox.clientsClaim();
