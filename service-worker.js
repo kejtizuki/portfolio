0c2bb374c64b24cb4624daf877f39ba31400a169
Kasia Zukowska <kasiazukowska@Kasias-MacBook-Pro.local>
2020-09-08 09:58:07 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2278cd6b3e9cca577b4b4f136f0c4bf7.js"
+  "/portfolio/precache-manifest.b7b7a4f2658e66920ba6ce5ca48c1ce3.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
