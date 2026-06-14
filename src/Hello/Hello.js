f4362993ceb87fcc0c029088357046925d3d5b32
kzukowska <kejtizuki@gmail.com>
2026-06-14 12:49:21 +0200

    hello

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -16,9 +16,9 @@ export default class Hello extends React.Component {
           <div className="hello-description">
             <p id="workLink" className='smallText'>
             I'm <a href="mailto:kejtizuki@gmail.com" className="linkHighlight">Kasia Żukowska</a> Polish
-            Product Designer based in Berlin. I enjoy building interactive experiences and digital products.
-            Currently building learning experience at <a href="https://www.babbel.com/" className="linkHighlight">Babbel</a>.
-            Before that I built accounts payables space at <a href="https://www.pleo.io/" className="linkHighlight">Pleo</a>. </p>
+            Product Designer based in Berlin. I love experimenting, thinking in systems and patterns and analysing problems.
+            Currently building a new learning experience at <a href="https://www.babbel.com/" className="linkHighlight">Babbel</a>.
+            Before that I built accounts payables space for finance teams at <a href="https://www.pleo.io/" className="linkHighlight">Pleo</a>. </p>
           </div>
         </div>
       </div>
